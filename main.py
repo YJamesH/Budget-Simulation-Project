88b2e5f3728d6337cecf2e9745625449083a37e4
James Hwang <yjamesh10@gmail.com>
2022-06-07 23:13:32 -0500

    Created main.py
    Added Inputs
    Added TCO functions - purchase and M&R
    Added Statistics Summary

---
+++ main.py
@@ -0,0 +1,99 @@
+from random import randrange
+
+
+
+# # INPUTS 
+# # Contract Inputs
+# contractTerm = int(input("Contract Term (years): ")) # Contract Term/Life of Bus
+# contractEsc = .03                                    # 3%/year
+# contractPrice = 30000                                # 30k bus/year
+
+# # User Info Inputs
+# annualBudget = int(input("Annual budget: "))          # Total Annual Budget ~60m
+# fleetSize = int(input("Total Fleet Size: "))          # Total Fleet Size 
+# annualMiles = int(input("Annual mileage with gas: ")) # Annual miles driven
+# weightedMPG = float(input("MPG with gas: "))          # Average mpg
+# MRCost = int(input("Maintenance & Repair Year 1 Base Cost: ")) # M&R base cost
+ 
+# # Diesel Info Inputs
+# dieselPrice = int(input("Diesel Bus Purchase Price: "))  # Price of 1 Diesel Bus ~120k
+# dieselRate = float(input("Diesel Bus Financing Rate: ")) # _%/year ~2%/year
+# dieselTerm = int(input("Diesel Bus Financing Term: "))   # Years to Finance Bus
+
+# dieselPriceEsc = .05                             # Diesel Price escalator 5%
+# overheadAllocation = .15                         # Overhead Cost Allocation 15%
+# costEsc = .02                                    # Other costs escalator (M&R, fuel, overhead,...)
+# MREsc1 = .06                                     # M&R escalator in first half-life
+# MREsc2 = .08                                     # M&R escalator in second half-life
+
+
+
+# --------------- coded inputs for testing --------------- #
+# Contract Inputs
+contractTerm = 15                                    # Contract Term/Life of Bus
+contractEsc = .03                                    # 3%/year
+contractPrice = 30000                                # 30k bus/year
+# User Info Inputs
+annualBudget = 60000000                               # Total Annual Budget ~60m
+fleetSize = 200                                       # Total Fleet Size 
+annualMiles = 9000                                    # Annual miles driven
+weightedMPG = 6                                       # Average mpg
+MRCost = 6000                                         # M&R base cost
+# Diesel Info Inputs
+dieselPrice = 120000                                  # Price of 1 Diesel Bus ~120k
+dieselRate = .03                                      # _%/year ~2%/year
+dieselTerm = 5                                        # Years to Finance Bus
+dieselPriceEsc = .05                             # Diesel Price escalator 5%
+overheadAllocation = .15                         # Overhead Cost Allocation 15%
+costEsc = .02                                    # Other costs escalator (M&R, fuel, overhead,...)
+MREsc1 = .06                                     # M&R escalator in first half-life
+MREsc2 = .08                                     # M&R escalator in second half-life
+# ------------- end coded inputs for testing ------------- #
+
+
+
+# -------------------------------------------------- #
+
+# Diesel Total Cost of Ownership (TCO)
+purchaseTCO = []
+mrTCO = []
+fuelTCO = []
+overheadTCO = []
+
+# WIP @@@@@
+# Purchase price with financing
+annualDieselBase = dieselPrice/dieselTerm
+for i in range(dieselTerm):
+    purchaseTCO.append(int(annualDieselBase*(pow(1+dieselRate,i))))
+
+print(purchaseTCO)
+
+# Maintenance and Repair
+for i in range(contractTerm):
+    # M&R differs in different half lifes
+    if i < (contractTerm-1)/2:
+        mrTCO.append(int(MRCost*(pow(1+MREsc1,i))))
+    else:
+        mrTCO.append(int(mrTCO[i-1]*(1+MREsc2)))
+
+print(mrTCO)
+
+# Fuel
+
+# Overhead
+
+
+
+
+
+# Statistics summary
+print("\n Summary Statistics")
+print("Annual Budget is ", annualBudget)
+print("Total Fleet Size is ", fleetSize)
+print("Annual mileage with gas is ", annualMiles)
+print("Gas Bus MPG is ", weightedMPG)
+print("Diesel Bus Purchase Price is ", dieselPrice)
+print("Diesel Bus Financing Rate is ", dieselRate)
+print("Diesel Bus Financing Term is ", dieselTerm)
+
+
